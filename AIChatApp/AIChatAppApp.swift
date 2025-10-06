863dfd71f44f729804de1dcbb64c625d2d3f51db
PREMSAIKVARMAOFFICIAL <premsaikvarma@outlook.com>
2025-10-06 12:50:46 +0530

    Initial Commit

---
+++ AIChatApp/AIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppApp.swift
+//  AIChatApp
+//
+//  Created by Prem Sai K Varma on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
